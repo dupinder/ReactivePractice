17a67858cdc8e507c5d8bd71b345a62350add974
dupinder <dupinderdhiman5@gmail.com>
2020-09-28 02:36:30 +0530

    Functional Handler and rouers with TestCases

---
+++ src/main/java/com/reactive/practice/routers/FirstRouterFunctionConfig.java
@@ -0,0 +1,28 @@
+package com.reactive.practice.routers;
+
+import com.reactive.practice.handlers.FirstHandlerFunction;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+
+@Configuration
+public class FirstRouterFunctionConfig {
+    @Bean
+    public RouterFunction<ServerResponse> router(FirstHandlerFunction firstHandlerFunction){
+        return RouterFunctions
+                .route(
+                        GET("/functional/flux").and(accept(MediaType.APPLICATION_JSON)),
+                        firstHandlerFunction::returnFlux
+                )
+                .andRoute(
+                        GET("/functional/mono").and(accept(MediaType.APPLICATION_JSON)),
+                        firstHandlerFunction::returnMono);
+    }
+
+}
